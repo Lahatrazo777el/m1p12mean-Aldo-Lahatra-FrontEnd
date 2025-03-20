48518d653631cc56316cda25c3fdc6fa8e847f54
lahatrazo <lahatrafate@gmail.com>
2025-03-20 20:45:20 +0300

    done changing home

--- src/app/pages/create-repair-history/create-repair-history.component.ts
+++ src/app/pages/create-repair-history/create-repair-history.component.ts
@@ -1,3 +1,4 @@
+import { VehiculeService } from '@/services/vehicule.service';
 import { RepairHistoryService } from './../../services/repair-history.service';
 import { FormsModule } from '@angular/forms';
 import { PrestationService } from './../../services/prestation.service';
@@ -5,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from '@/services/user.service';
 import { getTestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-create-repair-history',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './create-repair-history.component.html',
   styleUrl: './create-repair-history.component.css'
 })
@@ -16,9 +18,11 @@ export class CreateRepairHistoryComponent implements OnInit {
   prestations: any[] = [];
   clients: any[] = [];
   mecaniciens: any[] = [];
+  vehiculeClient: any[] = [];
+  errorMsg: string = '';
 
   history = {prestationId: '', clientId: '', mecanicienId: '', vehiculeId: ''}
-  constructor(private prestationService: PrestationService, private userService: UserService, private repairHistoryService: RepairHistoryService, private router: Router){}
+  constructor(private prestationService: PrestationService, private userService: UserService, private repairHistoryService: RepairHistoryService, private router: Router, private vehiculeService: VehiculeService){}
 
   ngOnInit(): void {
     this.loadPrestations(); 
@@ -39,7 +43,24 @@ export class CreateRepairHistoryComponent implements OnInit {
   }
 
   onSubmit(): void{
-    console.log(this.history);
-    this.repairHistoryService.addRepairHistory(this.history).subscribe(() => this.router.navigate(['/repair-histories']));
+    this.repairHistoryService.addRepairHistory(this.history).subscribe({
+      next: () => {
+        this.router.navigate(['/repair-histories']);
+      },
+      error: (error) => {
+        // Handle the error here
+        console.error('Error adding repair history:', error);
+
+        // this.errorMessage = 'Failed to add...';
+        if(error.status == 400){
+          this.errorMsg = error.error.message;
+        }
+      }
+    });
+  }
+
+  onClientChange(): void {
+
+    this.vehiculeService.findVehicule(this.history.clientId).subscribe((data) =>  this.vehiculeClient = data);
   }
 }
