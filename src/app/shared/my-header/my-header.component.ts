7cc3894dfed55fd27faca6f63e4e73aff84b870c
lahatra <lahatrafate@gmail.com>
2025-04-03 18:41:41 +0300

    solve dropdown bug

--- src/app/shared/my-header/my-header.component.ts
+++ src/app/shared/my-header/my-header.component.ts
@@ -24,7 +24,10 @@ export class MyHeaderComponent implements OnInit {
   constructor(public authService: AuthService, private router: Router) {}
 
   ngOnInit(): void{
-    
+    //initialize dropdown
+    this.isUserMenuOpen = false;
+    this.isMenuOpen = false;
+
     this.authSubscription = this.authService.isAuthenticated$.subscribe(
       (isAuthenticated) => {
         this.isLoggedIn = isAuthenticated;
